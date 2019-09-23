ff39cf724ea82a8cf0cdf4a641b3db014bc102ec
Misaki Koonce <misaki.koonce@gmail.com>
2019-09-23 01:45:33 -0700

    Initialize the project.

---
+++ PhoneBook.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+
+class Phonebook
+{
+  public static Dictionary<string, string> phoneNumbers = new Dictionary<string, string>();
+
+  static void Main()
+  {
+    //nothing here yet.
+  }
+
+  static void AddContact()
+  {
+    //nothing here yet.
+  }
+
+  static void LookUpContact()
+  {
+    //nothing here yet.
+  }
+}
