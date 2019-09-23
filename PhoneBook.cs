26c55c9df2627de1c27ccba7f70cd3d63990f754
Misaki Koonce <misaki.koonce@gmail.com>
2019-09-23 01:46:51 -0700

    Add contents to Main()

--- PhoneBook.cs
+++ PhoneBook.cs
@@ -6,10 +6,38 @@ class Phonebook
   public static Dictionary<string, string> phoneNumbers = new Dictionary<string, string>();
 
   static void Main()
+static void Main()
+{
+  Console.WriteLine("MAIN MENU");
+  Console.WriteLine("Would you like to add a person to your phone book? ['Y' for yes, 'Enter' for no]");
+  string answer = Console.ReadLine();
+  if (answer == "Y" || answer == "y")
   {
-    //nothing here yet.
+    AddContact();
   }
-
+  else
+  {
+    Console.WriteLine("Would you like to look up a number in your phone book? ['Y' for yes, 'Enter' for no]");
+    string lookUpAnswer = Console.ReadLine();
+    if (lookUpAnswer == "Y" || lookUpAnswer == "y")
+    {
+      LookUpContact();
+    }
+    else
+    {
+      Console.WriteLine("Are you finished with this program? ['Y' for yes, 'Enter' for no]");
+      string finishedAnswer = Console.ReadLine();
+      if (finishedAnswer == "Y" || finishedAnswer == "y")
+      {
+        Console.WriteLine("Goodbye.");
+      }
+      else
+      {
+        Main();
+      }
+    }
+  }
+}
   static void AddContact()
   {
     //nothing here yet.
