42de281c02b56ed32d840ff026bc7e9ad0f74698
Misaki Koonce <misaki.koonce@gmail.com>
2019-09-23 02:08:01 -0700

    Lookup a contact and display info.

--- PhoneBook.cs
+++ PhoneBook.cs
@@ -57,7 +57,19 @@ static void Main()
 }
 
   static void LookUpContact()
-  {
-    //nothing here yet.
+    {
+    Console.WriteLine("CONTACT LOOKUP");
+    Console.WriteLine("Whose number would you like to look up?");
+    string friend = Console.ReadLine();
+    if (phoneNumbers.ContainsKey(friend))
+    {
+      string value = phoneNumbers[friend];
+      Console.WriteLine(friend + "\'s phone number is " + value);
+    }
+    else
+    {
+      Console.WriteLine("That person is not in your phone book.");
+    }
+    Main();
   }
 }
